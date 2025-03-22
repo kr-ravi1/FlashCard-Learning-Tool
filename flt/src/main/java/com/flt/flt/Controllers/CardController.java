6a025b755c87635c3c12e827af96346c651a188d
Ravi Kumar <ravikr12082018@gmail.com>
2025-03-22 13:18:15 +0530

    Dockerfile updated

--- flt/src/main/java/com/flt/flt/Controllers/CardController.java
+++ flt/src/main/java/com/flt/flt/Controllers/CardController.java
@@ -5,7 +5,6 @@ import com.flt.flt.models.Categories;
 import com.flt.flt.models.Difficulties;
 import com.flt.flt.models.QuizCard;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
